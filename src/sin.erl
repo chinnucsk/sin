0c91b0badf800ba15fcffa2740d07e441d6fb9f3
Richard Giliam <nirosys@gmail.com>
2013-01-06 23:04:48 -0600

    Add sin.erl with basic proof of concept for Sin.
    I have a long ways to go before Sin is something that can be used
    to do what it is I want it to do. The more I learn about Erlang,
    the more I learn of what I should be doing. So this could take
    a few iterations before I get it right.

---
+++ src/sin.erl
@@ -0,0 +1,44 @@
+-module(sin).
+-author("Richard Giliam : http://github.com/nirosys").
+
+-export([main/0]).
+
+-on_load(init/0).
+
+-define(GENERAL_URL, "http://us.battle.net/d3/en/forum/3354739/").
+-define(PROFILE_BASE_URL, "http://us.battle.net/api/d3/profile/").
+
+init() ->
+    case application:start(inets) of
+        ok -> ok;
+        {error, {already_started, inets}} -> ok;
+        Error -> Error
+    end.
+
+main() ->
+    parseIndexPage(fetchUrl(?GENERAL_URL)).
+
+fetchUrl(Url) ->
+    io:format("Fetching: ~s~n", [Url]),
+    case httpc:request(Url) of
+        {ok, {_, _, Response}} -> Response;
+        {error, Reason} -> {error, Reason}
+    end.
+
+parseIndexPage(PageResponse) ->
+    Parsed = mochiweb_html:parse(PageResponse),
+    % Find Next button
+    % Find Posts..
+    [NextUrl, _] = mochiweb_xpath:execute("//li[@class='cap-item']/a/@href", Parsed),
+    fetchTopics(mochiweb_xpath:execute("//td[@class = 'post-title']/a/@href", Parsed)).
+
+fetchTopics([]) -> [];
+fetchTopics([Topic | Rest]) ->
+    %fetchTopic(Topic) + fetchTopics(Rest).
+    NewUrl = string:concat(?GENERAL_URL, binary_to_list(Topic)),
+    parseTopic(fetchUrl(NewUrl)) + fetchTopics(Rest).
+
+parseTopic(TopicResponse) ->
+    Parsed = mochiweb_html:parse(TopicResponse),
+    Profiles = mochiweb_xpath:execute("//a[@class='view-d3-profile']/@href", Parsed),
+    0.
